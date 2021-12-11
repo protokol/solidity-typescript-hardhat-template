193b82abcfed5a927505658f19b36eba98bfaf96
KovacZan <39158639+KovacZan@users.noreply.github.com>
2021-12-11 17:03:44 +0100

    chore: update all deps to latest (#67)

--- test/Test-Suite.ts
+++ test/Test-Suite.ts
@@ -1,4 +1,4 @@
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { Contract } from "ethers";
 import { ethers } from "hardhat";
 
