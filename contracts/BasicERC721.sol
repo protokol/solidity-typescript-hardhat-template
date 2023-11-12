66dd645690c727cf20fd82636afa364a20fe7b06
KovacZan <zan.kovi@proton.me>
2023-11-12 17:07:36 +0100

    feat: Setup solhin and solidity prettier

--- contracts/BasicERC721.sol
+++ contracts/BasicERC721.sol
@@ -1,14 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import {ERC721Pausable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
-import {ERC721Burnable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import { ERC721Enumerable } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import { ERC721Pausable } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+import { ERC721Burnable } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
-contract BasicERC721 is ERC721, ERC721Enumerable, ERC721Pausable, Ownable, ERC721Burnable {
+contract BasicERC721 is
+	ERC721,
+	ERC721Enumerable,
+	ERC721Pausable,
+	Ownable,
+	ERC721Burnable
+{
 	using Strings for uint256;
 
 	uint256 private _nextTokenId;
@@ -53,49 +59,50 @@ contract BasicERC721 is ERC721, ERC721Enumerable, ERC721Pausable, Ownable, ERC72
 		return _tokenBaseURI;
 	}
 
-	function contractURI() external view returns(string memory)  {
+	function contractURI() external view returns (string memory) {
 		return _contractURI;
 	}
 
 	/**
-	* @dev Returns base uri and adds .json suffix
-	* This is useful for metadata published on ipfs where files have .json suffix
-	*/
-	function tokenURI(uint256 tokenId)
-	public
-	view
-	override
-	returns (string memory)
-	{
-		if (ownerOf(tokenId) == address(0)) revert ERC721NonexistentToken(tokenId);
+	 * @dev Returns base uri and adds .json suffix
+	 * This is useful for metadata published on ipfs where files have .json suffix
+	 */
+	function tokenURI(
+		uint256 tokenId
+	) public view override returns (string memory) {
+		if (ownerOf(tokenId) == address(0))
+			revert ERC721NonexistentToken(tokenId);
 
 		return
 			bytes(_baseURI()).length > 0
-				? string(abi.encodePacked(_baseURI(), tokenId.toString(), ".json"))
+				? string(
+					abi.encodePacked(_baseURI(), tokenId.toString(), ".json")
+				)
 				: "";
 	}
 
-	function _update(address to, uint256 tokenId, address auth)
-	internal
-	override(ERC721, ERC721Enumerable, ERC721Pausable)
-	returns (address)
+	function _update(
+		address to,
+		uint256 tokenId,
+		address auth
+	)
+		internal
+		override(ERC721, ERC721Enumerable, ERC721Pausable)
+		returns (address)
 	{
 		return super._update(to, tokenId, auth);
 	}
 
-	function _increaseBalance(address account, uint128 value)
-	internal
-	override(ERC721, ERC721Enumerable)
-	{
+	function _increaseBalance(
+		address account,
+		uint128 value
+	) internal override(ERC721, ERC721Enumerable) {
 		super._increaseBalance(account, value);
 	}
 
-	function supportsInterface(bytes4 interfaceId)
-	public
-	view
-	override(ERC721, ERC721Enumerable)
-	returns (bool)
-	{
+	function supportsInterface(
+		bytes4 interfaceId
+	) public view override(ERC721, ERC721Enumerable) returns (bool) {
 		return super.supportsInterface(interfaceId);
 	}
 }
