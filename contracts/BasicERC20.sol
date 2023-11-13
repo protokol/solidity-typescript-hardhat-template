5e5d964723b63f9169de99551a068a5a3b318007
KovacZan <zan.kovi@proton.me>
2023-11-13 13:49:47 +0100

    fix: Format of ERC20

--- contracts/BasicERC20.sol
+++ contracts/BasicERC20.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { ERC20Burnable } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import { ERC20Pausable } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 
 contract BasicERC20 is ERC20, ERC20Burnable, ERC20Pausable, Ownable {
 	constructor(
@@ -13,15 +13,15 @@ contract BasicERC20 is ERC20, ERC20Burnable, ERC20Pausable, Ownable {
 		address initialOwner
 	) ERC20(name, symbol) Ownable(initialOwner) {}
 
-	function pause() public onlyOwner {
+	function pause() external onlyOwner {
 		_pause();
 	}
 
-	function unpause() public onlyOwner {
+	function unpause() external onlyOwner {
 		_unpause();
 	}
 
-	function mint(address to, uint256 amount) public onlyOwner {
+	function mint(address to, uint256 amount) external onlyOwner {
 		_mint(to, amount);
 	}
 
