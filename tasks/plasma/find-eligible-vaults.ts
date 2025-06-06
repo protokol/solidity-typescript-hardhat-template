895e2a8cdfa19addbf88ea40c1b5bc706df6c046
Kyle Yerman <kyleyerman99@gmail.com>
2025-06-06 11:42:53 -0600

    feat: :sparkles: Add total supply filter
    Filter out vaults which already have deposits

--- tasks/plasma/find-eligible-vaults.ts
+++ tasks/plasma/find-eligible-vaults.ts
@@ -2,6 +2,8 @@ import { task } from "hardhat/config"
 import * as fs from "fs"
 import * as path from "path"
 
+import boringVaultArtifact from "../../externalArtifacts/boringVault.json"
+
 /**
  Example:
  hardhat find-eligible-vaults \
@@ -9,10 +11,6 @@ import * as path from "path"
  */
 task("find-eligible-vaults", "Find vaults deployed by Veda")
 	.setAction(async (taskArgs, { ethers }) => {
-        scanVaults();
-})
-
-function scanVaults() {
     // Fetch vaults
     const filePath = path.resolve(__dirname, '../../data/scannedVaults.json');
     let data = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
@@ -21,11 +19,21 @@ function scanVaults() {
         data.scanned = [];
     }
 
-    let unsorted = []
-    for (let i = 0; i < data.unchecked.length; i++) {
-        data.scanned.push(data.unchecked[i]);
-        unsorted.push("Test")
+    if(!data.existingDeposits) {
+        data.existingDeposits = [];
     }
 
+    let newUnchecked = [];
+    for (let i = 0; i < data.unchecked.length; i++) {
+        const vaultContract = await ethers.getContractAt(boringVaultArtifact, data.unchecked[i]);
+        if ((await vaultContract.totalSupply()) > 0) {
+            data.existingDeposits.push(data.unchecked[i]);
+        } else {
+            newUnchecked.push(data.unchecked[i]);
+        }
+    }
+
+    data.unchecked = newUnchecked;
+
     fs.writeFileSync(filePath, JSON.stringify(data));
-}
+})
