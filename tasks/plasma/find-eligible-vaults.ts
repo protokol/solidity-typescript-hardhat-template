eb690c4246ec961807b89a2b501c93f3a7e5d3f0
Kyle Yerman <kyleyerman99@gmail.com>
2025-06-06 14:24:44 -0600

    feat: :sparkles: Add scanning for authorities
    Vaults don't know who their tellers are. The vault scanner will find active vaults, find authorities for those vaults, then scan through all blocks looking for active tellers.

--- tasks/plasma/find-eligible-vaults.ts
+++ tasks/plasma/find-eligible-vaults.ts
@@ -2,7 +2,7 @@ import { task } from "hardhat/config"
 import * as fs from "fs"
 import * as path from "path"
 
-import boringVaultArtifact from "../../externalArtifacts/boringVault.json"
+import boringVaultAbi from "../../externalArtifacts/boringVault.json"
 
 /**
  Example:
@@ -15,25 +15,66 @@ task("find-eligible-vaults", "Find vaults deployed by Veda")
     const filePath = path.resolve(__dirname, '../../data/scannedVaults.json');
     let data = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
 
-    if(!data.scanned) {
-        data.scanned = [];
+    initializeData(data);
+
+    // Sort unchecked vaults by whether they have existing deposits
+    for (let i = 0; i < data.unchecked.length; i++) {
+        const vaultContract = await ethers.getContractAt(boringVaultAbi, data.unchecked[i]);
+
+        // console.log('enter() function signature: ', vaultContract.interface.getFunction('enter')!.format());
+        // console.log('enter selector: ', vaultContract.interface.getFunction('enter')!.selector);
+        // throw new Error('done');
+        if ((await vaultContract.totalSupply()) > 0) {
+            console.log(`Moving vault ${data.unchecked[i]} to existing deposits`);
+            data.existingDeposits.push(data.unchecked[i]);
+        } else {
+            console.log(`Moving vault ${data.unchecked[i]} to valid unchecked vaults`);
+            data.validUncheckedVaults.push(data.unchecked[i]);
+        }
+    }
+    data.unchecked = [];
+
+
+    // Sort vaults without deposits by whether they have an authority set. Record authorities
+    if (data.validUncheckedVaults.length > 0) {
+        let authorityAddresses: Set<string> = new Set(data.authorityAddresses || []);
+        for (let i = 0; i < data.validUncheckedVaults.length; i++) {
+            const vaultContract = await ethers.getContractAt(boringVaultAbi, data.validUncheckedVaults[i]);
+            const authorityAddress = await vaultContract.authority();
+            if (authorityAddress.toLowerCase() === ethers.ZeroAddress.toLowerCase()) {
+                console.log(`Vault ${data.validUncheckedVaults[i]} has no authority set, moving to inactive vaults`);
+                data.inactiveVaults.push(data.validUncheckedVaults[i]);
+            } else {
+                console.log(`Vault ${data.validUncheckedVaults[i]} has authority set to ${authorityAddress}, checking for mint role`);
+                data.validCheckedVaults.push(data.validUncheckedVaults[i]);
+                authorityAddresses.add(authorityAddress);
+            }
+        }
+        data.validUncheckedVaults = [];
+        data.authorityAddresses = Array.from(authorityAddresses);
     }
 
+    fs.writeFileSync(filePath, JSON.stringify(data));
+})
+
+function initializeData(data: any) {
     if(!data.existingDeposits) {
         data.existingDeposits = [];
     }
 
-    let newUnchecked = [];
-    for (let i = 0; i < data.unchecked.length; i++) {
-        const vaultContract = await ethers.getContractAt(boringVaultArtifact, data.unchecked[i]);
-        if ((await vaultContract.totalSupply()) > 0) {
-            data.existingDeposits.push(data.unchecked[i]);
-        } else {
-            newUnchecked.push(data.unchecked[i]);
-        }
+    if (!data.validUncheckedVaults) {
+        data.validUncheckedVaults = [];
     }
 
-    data.unchecked = newUnchecked;
+    if (!data.validCheckedVaults) {
+        data.validCheckedVaults = [];
+    }
 
-    fs.writeFileSync(filePath, JSON.stringify(data));
-})
+    if (!data.unchecked) {
+        data.unchecked = [];
+    }
+
+    if (!data.inactiveVaults) {
+        data.inactiveVaults = [];
+    }
+}
