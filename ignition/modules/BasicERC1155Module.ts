b9d19f2f3772dd88e8827d20d6fdbe8a70155954
KovacZan <zan.kovi@proton.me>
2025-06-24 12:07:38 +0200

    feat: Use parameters file for config

--- ignition/modules/BasicERC1155Module.ts
+++ ignition/modules/BasicERC1155Module.ts
@@ -4,16 +4,17 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules"
  * BasicERC1155Module for deploying the BasicERC1155 token contract
  */
 const BasicERC1155Module = buildModule("BasicERC1155Module", (m) => {
+	// Contract parameters
+	const tokenName = m.getParameter("name", "Default Token Name")
+	const tokenSymbol = m.getParameter("symbol", "DTN")
+	const baseUri = m.getParameter("baseUri", "ipfs://base-uri/")
+	const contractUri = m.getParameter("contractUri", "ipfs://contract-uri")
 	// Account index 0 is the owner and deployer
 	const owner = m.getAccount(0)
 
-	const basicERC1155 = m.contract(
-		"BasicERC1155",
-		["ProtoToken", "PT", "ipfs://base-uri/", "ipfs://contract-uri", owner],
-		{
-			from: owner,
-		}
-	)
+	const basicERC1155 = m.contract("BasicERC1155", [tokenName, tokenSymbol, baseUri, contractUri, owner], {
+		from: owner,
+	})
 
 	return { basicERC1155 }
 })
