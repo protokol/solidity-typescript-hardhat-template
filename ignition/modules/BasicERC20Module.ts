ff28a677781658bf8713a69c8de41176be746824
KovacZan <zan.kovi@proton.me>
2025-06-24 12:04:14 +0200

    feat: Use parameters file for config

--- ignition/modules/BasicERC20Module.ts
+++ ignition/modules/BasicERC20Module.ts
@@ -4,10 +4,13 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules"
  * BasicERC20Module for deploying the BasicERC20 token contract
  */
 const BasicERC20Module = buildModule("BasicERC20Module", (m) => {
+	// Contract parameters
+	const tokenName = m.getParameter("name", "Default Token Name")
+	const tokenSymbol = m.getParameter("symbol", "DTS")
 	// Account index 0 is the owner and deployer
 	const owner = m.getAccount(0)
 
-	const basicERC20 = m.contract("BasicERC20", ["ProtoToken", "PT", owner], {
+	const basicERC20 = m.contract("BasicERC20", [tokenName, tokenSymbol, owner], {
 		from: owner,
 	})
 
